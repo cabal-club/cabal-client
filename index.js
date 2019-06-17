84e852a24daac83b9b61b29cb185b7aea696dac5
Bryan Clement <bclement@newrelic.com>
2019-06-16 22:25:25 -0700

    started moving logic from cabal into the client

--- index.js
+++ index.js
@@ -1,32 +1,71 @@
-var swarm = require('cabal-core/swarm.js')
+const Cabal = require('cabal-core')
+const swarm = require('cabal-core/swarm.js')
 
-function Client (cabal) {
-  if (!(this instanceof Client)) return new Client(cabal)
+class Client {
+  constructor(props) {
+    if (!(this instanceof Client)) return new Client(props)
+    this.cabals = new Map()
+    this.currentCabal = null
+    this.currentChannel = ''
+    this.maxFeeds = props.maxFeeds || 20 // TODO: what's a sane default?
+  }
 
-  this.connect = function () {
-    cabal.db.ready(function () {
-      swarm(cabal)
+  addCabal(key) {
+    return new Promise((resolve, reject) => {
+      // error states?
+      key = key.replace('cabal://', '').replace('cbl://', '').replace('dat://', '').replace(/\//g, '')
+      var db = this.archivesdir + key
+      var cabal = Cabal(db, key, { maxFeeds: this.maxFeeds })
+      cabal.ready(() => {
+        this.cabals.set(cabal, new CabalDetails())
+        cabal.swarm()
+        this.initializeCabalClient(cabal)
+        resolve(cabal)
+      })
     })
   }
 
-  this.getOpenChannels = function (channels) {
+  connect(cabal=this.currentCabal) {
+    cabal.db.ready(() => swarm(cabal))
+  }
+
+  getUsers(cabal=this.currentCabal) {
+    return this.cabals.get(cabal).users
+  }
+
+  getOpenChannels(cabal=this.currentCabal) {
+    return this.cabals.get(cabal).getChannels()
+  }
+
+  getUnreadMessages(channel) {
     // TODO
   }
 
-  this.getUnreadMessages = function (channel) {
+  openChannel(channel) {
     // TODO
   }
 
-  this.openChannel = function (channel) {
+  closeChannel(channel) {
     // TODO
   }
 
-  this.closeChannel = function (channel) {
+  markChannelRead(channel) {
     // TODO
   }
+}
 
-  this.markChannelRead = function (channel) {
-    // TODO
+class CabalDetails {
+  constructor() {
+    this.channels = {
+      '!status': '' // what should this hash be?
+    }
+    this.aliases = new Set()
+    this.users = {}
+    this.user = { local: true, online: true, key: '' }
+  }
+
+  getChannels() {
+    return Object.keys(this.channels)
   }
 }
 
